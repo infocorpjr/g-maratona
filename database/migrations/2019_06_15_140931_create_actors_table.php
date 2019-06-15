e6b88fac4db331f27ad3df79dacab4360b5c62f1
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-06-15 10:47:25 -0400

    Adicionado tabela de atores

---
+++ database/migrations/2019_06_15_140931_create_actors_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actors', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->comment("Chave primária para atores");
+            $table->integer('user_id')->unique()->unsigned()->comment("Chave estrangeira para usuário");
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            // Atenção! O padrão é ser participante ...
+            $table->boolean('is_administrator')->default(false)->comment("Determina se o ator é administrador");
+            $table->boolean('is_technician')->default(false)->comment('Determina se o ator é técnico');
+            $table->boolean('is_participant')->default(true)->comment('Determina se o ator é participante');
+            $table->boolean('is_voluntary')->default(false)->comment('Determina se o ator é voluntário');
+        });
+
+        Schema::table('actors', function (Blueprint $table) {
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actors');
+    }
+}
