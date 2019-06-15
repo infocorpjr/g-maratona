618149612629011dad37d4c311920dfb97a338c2
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-06-15 11:03:52 -0400

    Atualizado página inicial do cms

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,6 +48,11 @@
                 font-size: 84px;
             }
 
+            .subtitle {
+                margin: 0 0 35px 0;
+                font-size: 54px;
+            }
+
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -59,7 +64,7 @@
             }
 
             .m-b-md {
-                margin-bottom: 30px;
+
             }
         </style>
     </head>
@@ -81,17 +86,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Maratona de Programação
                 </div>
 
+                <h2 class="subtitle">Instituto de Computação UFMT</h2>
+
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Home</a>
+                    <a href="#">Galeria</a>
+                    <a href="#">Eventos</a>
+                    <a href="#">Sobre</a>
+                    <a href="#">Infocorp</a>
                 </div>
             </div>
         </div>
