fd587217e8744811cef059cd50b6fd6dccff30c4
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-06-16 13:30:25 -0400

    Corrigido erro no teste unitário

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::paginate(5);
+        $users = User::orderBy('created_at', 'desc')->paginate(5);
         return view('user.index')
             ->with('users', $users);
     }
